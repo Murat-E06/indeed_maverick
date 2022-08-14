e4b050bd09c0f0985445c47c4901460781525163
Murat <laserdancer06@gmail.com>
2022-08-14 14:27:54 +0300

    end of dataBase

---
+++ src/test/java/com/indeed/uk/step_definitions/Hooks.java
@@ -0,0 +1,27 @@
+package com.indeed.uk.step_definitions;
+
+import com.indeed.uk.utilities.DBUtils;
+import com.indeed.uk.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class Hooks {
+
+	@Before("@db")
+	public void dbHook() {
+		System.out.println("creating database connection");
+		DBUtils.createConnection();
+	}
+	
+	@After("@db")
+	public void afterDbHook() {
+		System.out.println("closing database connection");
+		DBUtils.destroy();
+	}
+	
+}
